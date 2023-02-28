61b33828549afba451d2894b170f5b22bfd661b1
Michael Heaton <Michael@Heatons.me>
2023-02-28 12:18:19 -0700

    Update OSX_Gaming.sh
    Added Signal install

--- macOS/OSX_Gaming.sh
+++ macOS/OSX_Gaming.sh
@@ -11,8 +11,16 @@ brew cask install twitch
 # Spotify
 brew cask install Spotify
 
+########################################################
+# Communication apps
+########################################################
 # Discord
 brew cask install Discord
+
+# Signal
+## URL: https://signal.org/
+## Brew: https://formulae.brew.sh/cask/signal
+brew install --cask signal
 
 ########################################################
 # Smart Home
