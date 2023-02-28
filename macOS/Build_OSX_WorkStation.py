e2560c339153ee45844096cd2e5f5ff100bc97c0
Michael Heaton <Michael@Heatons.me>
2023-02-28 12:19:15 -0700

    Create Build_OSX_WorkStation.py

---
+++ macOS/Build_OSX_WorkStation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Install Homebrew
+
+# Install MAS
+
+
+# Install my core system Utilitys
+
+# Install Productivey tools
+
+
+# Install DevSecOps tooling
+
+# Company system setup
+## Look into Intune for this.
+##  PacerPro
+### Install SecureFrame
+### URL: https://url5069.secureframe.com/ls/click?upn=nU8np3ZAm3tZwnGyDQXvRbnsySewD7CYwkDHvA4L8GpbVag-2Fsb2BRLsousHJ2iKPnkwGcOoK4xfra6-2FtoAX0wA-3D-3DyFvX_vDhyrByJyj9jzFVVCWkYCw750aK-2BWVT1dEcV3P1loxjba6ymkHIn8CBcXoTbFXSwVmi-2FbFGYqvc0XsBYvzSSMI8J27dRVOazH15yU5-2FAYlRpcTzYUbiP8vU4OmdesWO1CTQK-2BpB3g-2F8zv0Rbnxzt1AKex2ac11YntnEfTCT2eMexYkwDWI9ng9ja6lU8wiEEM-2Fp3I3oQquJAM8pSIQh72gp8qlsKo-2FUim9tjkXjB9ePMp-2FnG-2FDJv9D99BkmMxcEEpzM-2F-2BxiWNhcntYWQz8iwL5UA9PA9Gp5AAK8FPj3j8A5VUSVRq-2BVKzxrUgZcw8JY3
